b31fc056a902d68d86d4aeceb6eae61751a6c852
techno <elvisdiaz@chrislor-macbook.lan>
2026-05-26 02:12:58 -0400

    added maintenance gif

--- capshipping/settings.py
+++ capshipping/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'capshipping.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'capshipping',
+        'USER': 'capuser',
+        'PASSWORD': 'Capshipping@2026',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
