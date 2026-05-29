0712659ef45240a708feac1e97395a05f65d69d7
techno <elvisdiaz@chrislor-macbook.lan>
2026-05-29 13:46:22 -0400

    Switch to capshipp.com

--- capshipping/settings.py
+++ capshipping/settings.py
@@ -29,7 +29,13 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", False)
 
-ALLOWED_HOSTS = ['192.168.12.122',"127.0.0.1", "localhost", "0.0.0.0", "www.capshippingdistribution.com", "capshippingdistribution.com","capshipp.com","www.capshipp.com"]
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "0.0.0.0",
+    "capshipp.com",
+    "www.capshipp.com",
+]
 
 # Application definition
 
